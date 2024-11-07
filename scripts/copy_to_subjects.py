89ee59c6385ae2edf3780a48640f8d18376a0a72
Petr Jilek <petr.jilek156@gmail.com>
2024-11-07 18:56:25 +0100

    Setup pdf merging

--- scripts/copy_to_subjects.py
+++ scripts/copy_to_subjects.py
@@ -2,17 +2,11 @@ import os
 import shutil
 from pathlib import Path
 
-ROOT_FOLDER_PATH = Path(__file__).parent.parent
-
-SUBJECTS = "subjects"
-SUBJECTS_FOLDER_PATH = ROOT_FOLDER_PATH / SUBJECTS
-
-TEX = "tex"
-TEX_FOLDER_PATH = ROOT_FOLDER_PATH / TEX
+from shared import path_consts
 
 
 def copy_to_subjects(relative_path: str):
-    path = TEX_FOLDER_PATH / relative_path
+    path = path_consts.TEX_FOLDER_PATH / relative_path
 
     dirs = [d for d in os.listdir(path) if os.path.isdir(path / d)]
     for dir in dirs:
@@ -21,10 +15,10 @@ def copy_to_subjects(relative_path: str):
     pdfs = [f for f in os.listdir(path) if f.endswith(".pdf")]
     for pdf in pdfs:
         source = path / pdf
-        destination = SUBJECTS_FOLDER_PATH / relative_path / pdf
+        destination = path_consts.SUBJECTS_FOLDER_PATH / relative_path / pdf
 
-        if not os.path.exists(SUBJECTS_FOLDER_PATH / relative_path):
-            os.makedirs(SUBJECTS_FOLDER_PATH / relative_path)
+        if not os.path.exists(path_consts.SUBJECTS_FOLDER_PATH / relative_path):
+            os.makedirs(path_consts.SUBJECTS_FOLDER_PATH / relative_path)
 
         shutil.copy(source, destination)
 
---
+++ scripts/shared/path_consts.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+ROOT_FOLDER_PATH = Path(__file__).parent.parent.parent
+
+SUBJECTS = "subjects"
+SUBJECTS_FOLDER_PATH = ROOT_FOLDER_PATH / SUBJECTS
+
+TEX = "tex"
+TEX_FOLDER_PATH = ROOT_FOLDER_PATH / TEX
